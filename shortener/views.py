06a08e4b995a11c7ed4715e28c5bb349c6193b99
GreatRaksin <greatraksin@icloud.com>
2024-05-10 20:11:08 +0300

    Adding shortener main function (generate short link)

--- shortener/views.py
+++ shortener/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Links, Clicks
+from .forms import CreateUrlForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+import hashlib
+import string
 # Create your views here.
 
 
 def redirect_url(request, short_url):
+	print(short_url)
 	try:
 		url = Links.objects.get(short_url=short_url)
 	except Links.DoesNotExist:
 		return render(request, '404.html')
-	print(request.META)
 	url.clicks_qty += 1  # засчитываем один клик при переходе по ссылке
 	url.save()  # применить изменения в БД
 	click = Clicks(
@@ -18,4 +23,37 @@ def redirect_url(request, short_url):
 	)
 	click.save()
 	return redirect(url.original_url)
+
+
+#@login_required
+def create_short_link(request):
+	form = CreateUrlForm(request.POST)
+	user = request.user
+	if form.is_valid():
+		name = form.cleaned_data['name']
+		or_url = form.cleaned_data['original_url']
+		s_link = generate_short_url(str(user), or_url)
+		new_url = Links(
+			name=name,
+			user=User.objects.first(),
+			original_url=or_url,
+			short_url=s_link,
+		)
+		new_url.save()
+		return redirect('create-link')
+	return render(request, 'create-link.html', {'form': form})
+
+
+def generate_short_url(username, full_url):
+	# сгенерируем хеш из имени пользователя и оригинальной ссылки
+	hash_str = hashlib.sha256((username + full_url).encode('utf-8')).hexdigest()
+	# преобразуем хеш в base64, чтобы сделать ссылку короткой
+	short_url = ''
+	char_pool = string.digits + string.ascii_letters
+	for i in range(7):
+		index = int(hash_str[i * 2:i * 2 + 2], 16) % len(char_pool)
+		short_url += char_pool[index]
+
+	return short_url
+
 
